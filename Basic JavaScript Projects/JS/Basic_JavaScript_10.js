d9f74f2721f9b48ff60153cccfaa2ad1d9b579e6
Lara Foster <LaFoster@outlook.com>
2020-02-11 13:59:26 -0800

    Update JavaScript Folder
    Including Assignment for Step: 198 in Basic_HTML_10.html & Basic_JavaScript_10.js files.

--- Basic JavaScript Projects/JS/Basic_JavaScript_10.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10.js
@@ -1,7 +1,11 @@
-var text = "";
-var i;
-for (i = 20; i < 40; i++) {
-    if (i === 25) { continue; }
-    text += "The number is " + i + "<br>";
-}
-document.getElementById("count").innerHTML = text;
+let animal = {
+    species: "Dog ",
+    breed: "Olde English Bulldogge, ",
+    age: "3 yr old, ",
+    weight: "59 lbs, ",
+    sex: "female, ",
+    description:  function() {
+        return "The animal is a " + this.age + this.weight + this.breed + this.sex + this.species;
+    }
+};
+document.getElementById("Animal_Object").innerHTML = animal.description();
